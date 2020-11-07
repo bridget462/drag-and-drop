9e8af2be25b6acba2c31c5c0e4b712f75df2454f
bridget462 <bridges.dev.create@gmail.com>
2020-11-07 16:13:13 +0900

    new item can be added with add btn

--- script.js
+++ script.js
@@ -109,6 +109,15 @@ function updateDOM() {
   updateSavedColumns();
 }
 
+// add item to the column list, and reset the text box for next time
+function addToColumn(column) {
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = "";
+  updateDOM();
+}
+
 // show add item input box
 function showInputBox(column) {
   addBtns[column].style.visibility = "hidden";
@@ -121,6 +130,7 @@ function hideInputBox(column) {
   addBtns[column].style.visibility = "visible";
   saveItemBtns[column].style.display = "none";
   addItemContainers[column].style.display = "none";
+  addToColumn(column);
 }
 
 // allows arrays to reflect drag and rop items
