54be83ea9c2c8e42aead1477b43b2dc9268ae785
bridget462 <bridges.dev.create@gmail.com>
2020-11-07 10:32:03 +0900

    saved items are now loaded from LocalStorage

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const completeList = document.getElementById("complete-list");
 const onHoldList = document.getElementById("on-hold-list");
 
 // Items
+let updatedOnLoad = false; // to only load local storage once
 
 // Initialize Arrays
 let backlogListArray = [];
@@ -35,9 +36,6 @@ function getSavedColumns() {
   }
 }
 
-getSavedColumns();
-updateSavedColumns();
-
 // Set localStorage Arrays
 function updateSavedColumns() {
   listArrays = [
@@ -57,21 +55,51 @@ function updateSavedColumns() {
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
-  console.log("columnEl:", columnEl);
-  console.log("column:", column);
-  console.log("item:", item);
-  console.log("index:", index);
+  // console.log("columnEl:", columnEl);
+  // console.log("column:", column);
+  // console.log("item:", item);
+  // console.log("index:", index);
   // List Item
   const listEl = document.createElement("li");
   listEl.classList.add("drag-item");
+  listEl.textContent = item;
+  // Append
+  columnEl.appendChild(listEl);
 }
 
 // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
 function updateDOM() {
   // Check localStorage once
+  if (updatedOnLoad === false) {
+    getSavedColumns();
+  }
+
   // Backlog Column
+  backlogList.textContent = ""; // remove template strings
+  backlogListArray.forEach((backLogItem, index) => {
+    createItemEl(backlogList, 0, backLogItem, index);
+  });
+
   // Progress Column
+  progressList.textContent = ""; // remove template strings
+  progressListArray.forEach((progressItem, index) => {
+    createItemEl(progressList, 0, progressItem, index);
+  });
+
   // Complete Column
+  completeList.textContent = ""; // remove template strings
+  completeListArray.forEach((completeItem, index) => {
+    createItemEl(completeList, 0, completeItem, index);
+  });
+
   // On Hold Column
+  onHoldList.textContent = ""; // remove template strings
+  onHoldListArray.forEach((onHoldItem, index) => {
+    createItemEl(onHoldList, 0, onHoldItem, index);
+  });
+
   // Run getSavedColumns only once, Update Local Storage
 }
+
+// On Load
+updateDOM();
